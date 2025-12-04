efb2a90162b2048cf0122716d433cbef739c5f7e
saikumar <gorkesaikumar@gmail.com>
2025-12-04 20:14:59 +0530

    added template config

--- coaching_system/settings.py
+++ coaching_system/settings.py
@@ -36,6 +36,22 @@ INSTALLED_APPS = [
 
 AUTH_USER_MODEL = 'core.User'
 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / "templates"],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
