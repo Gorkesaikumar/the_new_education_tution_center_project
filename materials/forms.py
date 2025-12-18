639e484b5d10ed21008ec29aa3c5c88c7cd8ba49
saikumar <gorkesaikumar@gmail.com>
2025-12-18 11:35:50 +0530

    22

--- materials/forms.py
+++ materials/forms.py
@@ -1,51 +1,27 @@
 from django import forms
 from .models import StudyMaterial
-from batches.models import Batch, Subject
-
 
 class StudyMaterialForm(forms.ModelForm):
     class Meta:
         model = StudyMaterial
-        fields = ['title', 'description', 'file', 'batch', 'subject']
+        fields = ["title", "description", "file", "batch", "subject"]
+
         widgets = {
             "title": forms.TextInput(attrs={
-                "class": "form-control",
-                "placeholder": "Enter material title"
+                "class": "form-control bg-light border-0"
             }),
             "description": forms.Textarea(attrs={
-                "class": "form-control",
-                "rows": 3,
-                "placeholder": "Optional description"
+                "class": "form-control bg-light border-0",
+                "rows": 3
             }),
             "file": forms.ClearableFileInput(attrs={
-                "class": "form-control",
+                "class": "form-control bg-light border-0",
                 "accept": ".pdf,.doc,.docx,.ppt,.pptx,.jpg,.jpeg,.png"
             }),
             "batch": forms.Select(attrs={
-                "class": "form-select"
+                "class": "form-control bg-light border-0"
             }),
             "subject": forms.Select(attrs={
-                "class": "form-select"
+                "class": "form-control bg-light border-0"
             }),
         }
-
-    def __init__(self, *args, **kwargs):
-        user = kwargs.pop("user", None)
-        super().__init__(*args, **kwargs)
-
-        # Always restrict subjects until batch is selected
-        self.fields["subject"].queryset = Subject.objects.none()
-
-        if self.instance.pk and self.instance.batch:
-            self.fields["subject"].queryset = Subject.objects.filter(
-                batch=self.instance.batch
-            )
-
-        if "batch" in self.data:
-            try:
-                batch_id = int(self.data.get("batch"))
-                self.fields["subject"].queryset = Subject.objects.filter(
-                    batch_id=batch_id
-                )
-            except (ValueError, TypeError):
-                pass
